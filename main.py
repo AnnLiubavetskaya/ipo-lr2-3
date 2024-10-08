422ca165b5b8f2aac7e7d56af1539361239dcd56
Lyap <20anita07l@gmail.com>
2024-10-08 12:02:13 +0300

    Update main.py
    .

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ import math
 x = int(input("Введите x: "))
 y = int(input("Введите y: "))
 z = float(input("Введите z от -1 до 1: "))
-b = math.sqrt(10 * (x * (1. / 3.) + x * (y + 2))) * ((math.asin(z) ** 2) - abs(x - y))
+b = math.sqrt(10 * (x * (1 / 3) + x * (y + 2))) * ((math.asin(z) ** 2) - abs(x - y))
 print('Ответ: ')
 print(b)
